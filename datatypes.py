8685857affdd8db583e19468a1c4e40c43cf8215
tejasri <tejasrit2027@gmail.com>
2025-06-20 14:51:02 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,42 @@
+a=15
+print(a)
+print(type(a)) 
+print(id(a))
+
+a= 15.28
+print(a)
+print(type(a))
+print(id(a))
+
+a= 5+2j
+print(a)
+print(type(a))
+print(id(a))
+
+a="ramcharan"
+print(a)
+print(type(a))
+print(id(a))
+
+mylist = [28,28.15,29+15j,"teju"]
+print(mylist)
+print(type(mylist))
+print(id(mylist))
+
+mytuple = (28,28.15,29+15j,"teju")
+print(mytuple)
+print(type(mytuple))
+print(id(mytuple))
+
+a= True
+print(a)
+print(type(a))
+
+myset={26, 26.5, 23+5j, "sri"}
+print(myset)
+print(type(myset))
+print(id(myset))
+
+mydic={"name":'tejasri'}
+print(mydic)
+print(type(mydic))
